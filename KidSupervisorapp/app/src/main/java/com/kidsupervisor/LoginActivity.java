76780e9c8b66ccd2554b52171514b99c2002b03c
melch9 <122480996+melch9@users.noreply.github.com>
2023-02-18 14:42:11 -0500

    Add files via upload

---
+++ KidSupervisorapp/app/src/main/java/com/kidsupervisor/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.kidsupervisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.kidsupervisor.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+        binding.newAccountTextViw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+                finish();
+            }
+        });
+
+
+
+    }
+}
