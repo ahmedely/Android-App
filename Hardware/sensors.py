17b730d94c4d9f8808117e0f6a07b85b77e0200f
ahmedely <63077422+ahmedely@users.noreply.github.com>
2023-02-18 19:22:10 -0500

    Update sensors.py

--- Hardware/sensors.py
+++ Hardware/sensors.py
@@ -1,12 +1,25 @@
 import RPi.GPIO as GPIO
-import time
-movement=21
+from time import sleep
+
+movement=21 #sensor pin
+gate=18 #audio pin
+counter=int(0)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(movement,GPIO.IN)
-while True:
-  readVal=GPIO.input(movement)
-  if readVal==0:
-    print("Motion not detected")
-  else:
-    print("Motion detected")
-GPIO.cleanup()
+GPIO.setup(gate,GPIO.IN)
+try:
+  while True:
+    readSound=GPIO.input(gate)
+    getMove=GPIO.input(movement)
+    if readSound==1:
+      print("Sound detected")
+    else:
+      print("No sound")
+    if getMove==0:
+      print("Movement not detected")
+      sleep(2)
+      counter=0
+    else:
+      print("Motion detected")
+      sleep(2)
