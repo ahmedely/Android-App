a9a29d5f14c23269d218843ff4c7b5d5c2791dfb
Abdoullah <abdoullah.ayadi@aheeva.com>
2023-02-23 20:25:42 -0500

    Add signUp and Login with Firebase

--- KidSupervisorapp/app/src/main/java/com/kidsupervisor/LoginActivity.java
+++ KidSupervisorapp/app/src/main/java/com/kidsupervisor/LoginActivity.java
@@ -1,41 +1,69 @@
 package com.kidsupervisor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.kidsupervisor.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-
-
-
-    ActivityLoginBinding binding;
+    private ActivityLoginBinding binding;
+    private String email;
+    private String password;
+    private TextView txt_email;
+    private TextView txt_password;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        auth = FirebaseAuth.getInstance();
+        txt_email = findViewById(R.id.email);
+        txt_password = findViewById(R.id.password);
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                email = txt_email.getText().toString();
+                password = txt_password.getText().toString();
+
+                signIn(email, password);
+
             }
         });
         binding.newAccountTextViw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this,SignupActivity.class));
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
                 finish();
             }
         });
-
-
+    }
+    private void signIn(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                } else {
+                    Toast.makeText(LoginActivity.this, "Email or Password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
