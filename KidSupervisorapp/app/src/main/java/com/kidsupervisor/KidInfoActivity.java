b69a9d0837109667f07dd3b11eb1d54822214f16
melch9 <122480996+melch9@users.noreply.github.com>
2023-03-17 18:42:26 -0400

    Settings
    Dark mode, Enable/Disable notifications , sign out implemented and help button.

--- KidSupervisorapp/app/src/main/java/com/kidsupervisor/KidInfoActivity.java
+++ KidSupervisorapp/app/src/main/java/com/kidsupervisor/KidInfoActivity.java
@@ -1,10 +1,10 @@
 package com.kidsupervisor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class KidInfoActivity extends AppCompatActivity {
 
@@ -12,9 +12,20 @@ public class KidInfoActivity extends AppCompatActivity {
 
     }
 
+
+    Pref pref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        pref = new Pref(this);
+
+        if (!pref.getBoolean("Switch")) {
+            setTheme(R.style.lighttheme);
+        } else {
+            setTheme(R.style.darktheme);
+        }
         setContentView(R.layout.activity_kid_info);
 
         Button saveBut, editBut, deleteBut;
